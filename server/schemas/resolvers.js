d3e0dde99409c19b1ce5a18234477f86c9fb2cfd
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-07 17:45:49 -0700

    login added to mutations and tested on the backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,6 +19,22 @@ const resolvers = {
             const user = await User.create({ username, email, password })
             const token = signToken(user)
             return { token, user }
+        },
+        login: async (parent, { email, username, password }) => {
+            const user = await User.findOne({ $or: [{ username: username }, { email: email }] })
+            // if no user is found, throw error 
+            if (!user) {
+                throw AuthenticationError
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            //if password doesn't match throw error
+            if (!correctPw) {
+                throw AuthenticationError
+            }
+
+            const token = signToken(user);
+            return { token, user }
         }
     }
 }
