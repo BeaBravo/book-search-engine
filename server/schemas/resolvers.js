df3e16266385d6ef5cd620262b4e3588ce0a1d7d
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-07 17:33:45 -0700

    added mutation to createUser and tested it

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,24 @@
 const { User } = require('../models')
+const { signToken, AuthenticationError } = require('../utils/auth')
 
 const resolvers = {
     Query: {
         users: async () => {
             return User.find().populate('savedBooks')
+        },
+
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id })
+            }
+            throw AuthenticationError;
+        }
+    },
+    Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password })
+            const token = signToken(user)
+            return { token, user }
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,34 @@
 const typeDefs = `
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+
+    }
     type User {
         _id: ID
         username: String
         email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
     
     type Query {
-        users: [User]
+        users: [User]!
+        me: User
+    }
+    
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        login(email: String, username: String, password: String!): Auth
     }`
 
 module.exports = typeDefs
